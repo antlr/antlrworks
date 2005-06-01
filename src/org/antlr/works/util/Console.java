60b2c47bf6daf790abd067d263384d9c5b5cf373
Jean Bovet <jean_bovet@mac.com>
2005-05-31 22:09:29 -0800

    - new Console - new ErrorListener
    [git-p4: depot-paths = "//depot/code/antlrworks/main/": change = 1761]

---
+++ src/org/antlr/works/util/Console.java
@@ -0,0 +1,70 @@
+/*
+
+[The "BSD licence"]
+Copyright (c) 2005 Jean Bovet
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package org.antlr.works.util;
+
+import edu.usfca.xj.appkit.frame.XJPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Console extends XJPanel {
+
+    protected static Console shared;
+    protected JTextArea textArea;
+
+    public synchronized static Console shared() {
+        if(shared == null)
+            shared = new Console();
+        return shared;
+    }
+
+    public Console() {
+        super();
+
+        textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setWheelScrollingEnabled(true);
+
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+        setTitle("Console");
+        setSize(400, 300);
+
+        awake();
+    }
+
+    public synchronized void println(String s) {
+        System.out.println("Console: "+s);
+        textArea.setText(textArea.getText()+s+"\n");
+    }
+}
